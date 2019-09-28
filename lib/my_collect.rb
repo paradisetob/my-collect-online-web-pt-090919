139654e0cad53f7fc650966b569f53fb793f4d5d
tracey brimage <onelovenilynn@gmail.com>
2019-09-28 03:09:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
   name.split(" ").first
 end
-ollection = ['ruby', 'javascript', 'python', 'objective-c']
+
+Collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
   lang.upcase
 end
- 
