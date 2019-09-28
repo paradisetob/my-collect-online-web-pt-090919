d9ae8008dd2853e2dc6b1c6e6013e1adc0dd7b65
tracey brimage <onelovenilynn@gmail.com>
2019-09-28 03:10:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
-
+Collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
